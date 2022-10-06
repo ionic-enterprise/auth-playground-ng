f79b95927679fc22a55c0e5c7c0c6225e46d29d1
Ken Sodemann <ken@ionic.io>
2022-10-06 16:35:03 -0500

    fix(vault): default privacy screen to true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Device } from '@ionic-enterprise/identity-vault';
 import { NavController } from '@ionic/angular';
 import { SessionVaultService } from './core';
 
@@ -7,12 +8,16 @@ import { SessionVaultService } from './core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor(navController: NavController, vault: SessionVaultService) {
-    vault.locked.subscribe((lock: boolean) => {
+export class AppComponent implements OnInit {
+  constructor(private navController: NavController, private vault: SessionVaultService) {}
+
+  ngOnInit(): void {
+    this.vault.locked.subscribe((lock: boolean) => {
       if (lock) {
-        navController.navigateRoot(['/', 'unlock']);
+        this.navController.navigateRoot(['/', 'unlock']);
       }
     });
+
+    Device.setHideScreenOnBackground(true);
   }
 }
