da7ec3b15aee020bf0e8ff4d21c78eaef31fc315
Ken Sodemann <ken@ionic.io>
2023-03-07 16:01:38 -0600

    chore(): upgrade to the GA version of AC

--- src/app/core/authentication/oidc-authentication/oidc-authentication.service.spec.ts
+++ src/app/core/authentication/oidc-authentication/oidc-authentication.service.spec.ts
@@ -57,7 +57,7 @@ const testAuthResult = {
           webView: 'private',
         },
         web: {
-          implicitLogin: 'popup',
+          uiMode: 'popup',
           authFlow: 'implicit',
         },
       });
--- src/app/core/authentication/oidc-authentication/oidc-authentication.service.ts
+++ src/app/core/authentication/oidc-authentication/oidc-authentication.service.ts
@@ -118,7 +118,7 @@ export class OIDCAuthenticationService implements Authenticator {
         webView: 'private',
       },
       web: {
-        implicitLogin: 'popup',
+        uiMode: 'popup',
         authFlow: 'implicit',
       },
     });
